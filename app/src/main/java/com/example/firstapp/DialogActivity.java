a060ea94d080622430b37d6b3cb8b864dab96f33
Priyanshu <kumbhkarprinshu339@gmail.com>
2020-12-30 22:09:37 +0530

    30.12.2020 10:09

--- app/src/main/java/com/example/firstapp/DialogActivity.java
+++ app/src/main/java/com/example/firstapp/DialogActivity.java
@@ -1,6 +1,5 @@
 package com.example.firstapp;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,44 +7,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.text.format.Time;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-//import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.mlkit.vision.common.InputImage;
-import com.google.mlkit.vision.text.Text;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.google.mlkit.vision.text.TextRecognition;
-import com.google.mlkit.vision.text.TextRecognizer;
 import com.theartofdev.edmodo.cropper.CropOverlayView;
 
 import java.io.File;
-import java.io.IOException;
 
 public class DialogActivity extends AppCompatActivity {
-//    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    ImageButton btBrowse, btReset, btDone;
+    ImageButton btBrowse, btReset;
     ImageView imageView;
     Uri uri;
-    private FirebaseStorage storage;
-    private InputImage image;
-    TextRecognizer recognizer = TextRecognition.getClient();
-    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +33,8 @@ public class DialogActivity extends AppCompatActivity {
         final Uri imageuri = Uri.fromFile(new File(sspath));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
-        storage = FirebaseStorage.getInstance();
         btBrowse= findViewById(R.id.bt_edit);
         btReset=findViewById(R.id.bt_reset);
-        btDone=findViewById(R.id.bt_done);
         imageView=findViewById(R.id.image_view);
         imageView.setImageURI(imageuri);
 
@@ -80,12 +57,6 @@ public class DialogActivity extends AppCompatActivity {
                 imageView.setImageBitmap(null);
             }
         });
-        btDone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finishAffinity();
-            }
-        });
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -93,34 +64,9 @@ public class DialogActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
             CropImage.ActivityResult result= CropImage.getActivityResult(data);
-            imageView.setImageURI(result.getUri());
-            Toast.makeText(this, "Image update Successfully!!!",
-                    Toast.LENGTH_SHORT).show();
-
-            try {
-                image = InputImage.fromFilePath(getBaseContext(), result.getUri());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            recognizer.process(image)
-                    .addOnSuccessListener(
-                            new OnSuccessListener<Text>() {
-                                @Override
-                                public void onSuccess(Text texts) {
-                                    text= texts.getText();
-                                }
-                            })
-                    .addOnFailureListener(
-                            new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    // Task failed with an exception
-                                    e.printStackTrace();
-                                }
-                            });
-
-            Toast.makeText(getBaseContext(), "text:"+text, Toast.LENGTH_LONG).show();
-
+                imageView.setImageURI(result.getUri());
+                Toast.makeText(this, "Image update Successfully!!!",
+                        Toast.LENGTH_SHORT).show();
         }
     }
 
